d43716157eb52ede79d24a31f93a08f200744980
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-08-14 16:19:13 +0300

    1

---
+++ src/js/03-promises.js
@@ -0,0 +1,19 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
