343709f9a03398ce8250e1d4c6131abfd01d21b2
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-08-17 16:48:52 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,42 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  let counter = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, counter)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    counter += Number(step.value);
   }
+  clearInput();
+});
+
+function clearInput() {
+  counter = Number(delay.value);
+
+  // delay.value = '';
+  // step.value = '';
+  // amount.value = '';
 }
